b8fef27e4d4449392ebc2f807e48a2870dd71c7d
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-09-22 06:31:01 +0000

    fix routes err

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -47,4 +47,3 @@ class Api::V1::TasksController < ApplicationController
   end
 end
 
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :spi do
+  namespace :api do
     namespace :v1, format: :json do
       resources :tasks
     end
